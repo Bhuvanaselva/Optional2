1876ea941fd106332484cb9c9be46bfb766fad2d
¨Bhuvana¨ <bhuvanaselvaraj25@gmail.com>
2023-09-29 09:39:59 +0200

    Add project files.

---
+++ Optional2/Class1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+
+namespace Optional2
+{
+    internal class Class1
+    {
+        //Ticket Office Assignment
+        public static int GetAge()
+        {
+            Console.WriteLine("Please enter your age: ");
+            string text = Console.ReadLine();
+            int age = Convert.ToInt32(text);
+            return age;
+        }
+
+        public static string GetTicket()
+        {
+            Console.WriteLine("\nSelect Standing or Seated ticket: ");
+            string place = Console.ReadLine();
+            if (place != "standing" && place != "seated")
+            {
+                Console.WriteLine("Invalid input. Please enter 'standing' or 'seated'.");
+                return GetTicket();
+            }
+            return place;
+        }
+        public static int PriceSetter(int age, string place)
+        {
+            int price;
+
+            if (age <= 11)
+            {
+                price = (place == "standing") ? 25 : 50;
+            }
+            else if (age >= 12 && age <= 64)
+            {
+                price = (place == "standing") ? 110 : 170;
+            }
+            else
+            {
+                price = (place == "standing") ? 60 : 100;
+            }
+
+            return price;
+        }
+        public static decimal TaxCalculator(int price)
+        {
+            decimal tax = Convert.ToDecimal((1 - 1 / 1.06) * price);
+            return Math.Round(tax, 2);
+        }
+
+        public static int TicketNumberGenerator()
+        {
+            var rand = new Random();
+            return rand.Next(1, 8000);
+        }
+
+        // Optional Assignment 1
+
+        public static bool CheckPlaceAvailability(string placeList, int placeNumber)
+        {
+
+            string searchPattern = $",{placeNumber},";
+            return !placeList.Contains(searchPattern);
+        }
+
+        public static string AddPlace(string placeList, int placeNumber)
+        {
+            placeList += $"{placeNumber},";
+            return placeList;
+        }
+
+        // Optional Assignment 2
+
+        public static int GetCustomerAge()
+        {
+            int Age;
+            string input;
+
+            do
+            {
+                Console.WriteLine("Enter customer's age: ");
+                input = Console.ReadLine();
+
+            } while (!IsNumeric(input) || !IsAgeValid(input, out Age));
+
+            return Age;
+        }
+
+        static bool IsNumeric(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!Char.IsDigit(c))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.\n");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsAgeValid(string input, out int Age)
+        {
+            if (int.TryParse(input, out Age) && input.Length >= 1 && input.Length <= 3)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid age. Age must be between 1 and 3 characters in length.\n");
+                return false;
+            }
+        }
+
+        public static string GetCustomerPlacePreference()
+        {
+            string preference;
+            bool validInput = false;
+
+            do
+            {
+                Console.Write("Enter customer's place preference (Seated or Standing): ");
+                preference = Console.ReadLine();
+
+                if (preference.Equals("Seated", StringComparison.OrdinalIgnoreCase) ||
+                    preference.Equals("Standing", StringComparison.OrdinalIgnoreCase))
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter Seated or Standing.");
+                }
+            } while (!validInput);
+
+            return preference;
+        }
+    }
+}
+
+       
---
+++ Optional2/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Optional2;
+
+//Ticket Office Assignment
+
+Console.WriteLine("Welcome to the Ticket Booking!\n");
+int age = Class1.GetAge();
+string place = Class1.GetTicket();
+int price = Class1.PriceSetter(age, place);
+decimal tax = Class1.TaxCalculator(price);
+int ticketNumber = Class1.TicketNumberGenerator();
+Console.WriteLine("\nTicket Details:");
+Console.WriteLine($"Age: {age} years");
+Console.WriteLine($"Place preference: {place}");
+Console.WriteLine($"Total Price: {price} SEK");
+Console.WriteLine($"Tax (6%): {tax} SEK");
+Console.WriteLine($"Ticket Number: {ticketNumber}\n");
+
+// Optional Assignment 1
+
+string placeList = ",34,1003,389,4100,4890,7233,2855,";
+
+Console.WriteLine("Please choose PlaceNumber if you have 'seated' ticket or 'Exit' \n");
+
+while (true)
+{
+
+    Console.WriteLine("1. Check Place Availability");
+    Console.WriteLine("2. Add a Place");
+    Console.WriteLine("3. Customer Preference");
+    Console.WriteLine("4. Exit");
+    Console.WriteLine("\nEnter your choice (1/2/3/4): ");
+
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Console.WriteLine("\nEnter Place Number: ");
+            int placeNumber = Convert.ToInt32(Console.ReadLine());
+            bool isAvailable = Class1.CheckPlaceAvailability(placeList, placeNumber);
+
+            if (isAvailable)
+            {
+                Console.WriteLine($"Place {placeNumber} is available:{isAvailable}\n");
+            }
+            else
+            {
+                Console.WriteLine($"Place {placeNumber} is sold :{isAvailable}");
+                Console.WriteLine("\nSold Places: ");
+                Console.WriteLine(placeList + "\n");
+            }
+            break;
+
+        case 2:
+            Console.WriteLine("\nEnter Place Number to add: ");
+            int placeToAdd = Convert.ToInt32(Console.ReadLine());
+            placeList = Class1.AddPlace(placeList, placeToAdd);
+            Console.WriteLine("Place {0} has been added successfully.", placeToAdd);
+            Console.WriteLine("\nUpdated PlaceList: ");
+            Console.WriteLine(placeList + "\n");
+            break;
+
+            //Case 3 is Optional Assignment 2
+
+        case 3:
+            Console.WriteLine("\n Welcome!!\n");
+
+            int Age = Class1.GetCustomerAge();
+            Console.WriteLine($"Customer's age is: {Age}\n");
+
+            string preference = Class1.GetCustomerPlacePreference();
+            Console.WriteLine($"Customer's place preference is: {preference}\n");
+            Console.WriteLine("Thank you for providing the information!\n");
+            break;
+
+        case 4:
+            Console.WriteLine("Thank you for booking!!");
+            return;
+
+        default:
+            Console.WriteLine("Invalid Choice. Please try again.");
+            break;
+
+    }
+}
+
+
+
